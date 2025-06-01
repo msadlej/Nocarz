2ea6f45ef66f0dfdc426f43b7ec7fc4735c90cd7
msadlej <01178587@pw.edu.pl>
2025-06-01 14:22:10 +0200

    update: data columns

--- nocarz/api/schemas.py
+++ nocarz/api/schemas.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel
 
 
 class ListingResponse(BaseModel):
+    property_type: str
+    room_type: str
+    bathrooms_text: str
     accommodates: int
     bathrooms: int
     bedrooms: int
     beds: int
     price: float
-    property_type: str
-    room_type: str
-    bathrooms_text: str
