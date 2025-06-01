601555df041e0e74611173680c9a03bdbeb87d58
msadlej <01178587@pw.edu.pl>
2025-06-01 17:03:47 +0200

    update: prediction logging

--- nocarz/api/utils.py
+++ nocarz/api/utils.py
@@ -2,6 +2,7 @@ from nocarz.config import MODELS_DIR, LOGS_DIR
 from nocarz.api.schemas import ListingResponse
 from nocarz.src.base_model import BaseModel
 from datetime import datetime
+import pandas as pd
 
 
 def get_base_prediction(host_id: int) -> ListingResponse:
@@ -9,18 +10,31 @@ def get_base_prediction(host_id: int) -> ListingResponse:
     base_model.load(MODELS_DIR / "base_model.pkl")
 
     pred, type = base_model.predict(host_id)
-
-    with open(LOGS_DIR / "logs.csv", "a") as f:
-        timestamp = datetime.now().strftime('%d/%m/%Y %H:%M:%S')
-        f.write(f"{timestamp},base,{host_id},{pred},{type}\n")
-
-    return ListingResponse(
+    result = ListingResponse(
+        property_type=pred["property_type"],
+        room_type=pred["room_type"],
+        bathrooms_text=pred["bathrooms_text"],
         accommodates=pred["accommodates"],
         bathrooms=pred["bathrooms"],
         bedrooms=pred["bedrooms"],
         beds=pred["beds"],
         price=pred["price"],
-        property_type=pred["property_type"],
-        room_type=pred["room_type"],
-        bathrooms_text=pred["bathrooms_text"],
     )
+
+    log = pd.DataFrame({
+        'timestamp': [datetime.now().strftime('%d/%m/%Y %H:%M:%S')],
+        'model': ['base'],
+        'type': [type],
+        'host_id': [host_id],
+        'property_type': [result.property_type],
+        'room_type': [result.room_type],
+        'bathrooms_text': [result.bathrooms_text],
+        'accommodates': [result.accommodates],
+        'bathrooms': [result.bathrooms],
+        'bedrooms': [result.bedrooms],
+        'beds': [result.beds],
+        'price': [result.price]
+    })
+    log.to_csv(LOGS_DIR / "logs.csv", mode='a', header=False, index=False)
+
+    return result
