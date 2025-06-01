629f08fe60aadc1f1e567feb84ed0ef2398cce41
msadlej <01178587@pw.edu.pl>
2025-06-01 13:42:54 +0200

    add: server settings

--- nocarz/api/main.py
+++ nocarz/api/main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+import uvicorn
+
+from nocarz.config import HOST, PORT
 
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return Response("Server is running.")
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host=HOST,
+        port=PORT,
+        reload=True,
+    )
--- nocarz/config.py
+++ nocarz/config.py
@@ -7,3 +7,6 @@ DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 MODELS_DIR = PROJ_ROOT / "models"
+
+HOST = "0.0.0.0"
+PORT = 8000
